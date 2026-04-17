dfbbcdae0e6109c4ef7b61ffaf05833bbbbaa0a3
Dmitriy0212 <demarr68@gmail.com>
2026-04-17 21:04:56 +0300

    fix

---
+++ src/components/Notification/Notification.tsx
@@ -0,0 +1,9 @@
+import css from "./Notification.module.css";
+
+export default function VoteStats() {
+  return (
+    <div>
+      <p className={css.message}>No feedback yet</p>
+    </div>
+  );
+}
